3d77dd3f4defe41eeb6a83c8b271312de50a7eec
Steven Goodgrove <steven.goodgrove@imosphere.co.uk>
2018-11-15 12:20:17 +0000

    Source code commit.

---
+++ src/TrivialBDD/TrivialBDD.cs
@@ -0,0 +1,10 @@
+﻿namespace TrivialBDD
+{
+    public static class BDD
+    {
+        public static T Given<T>(this T t) => t;
+        public static T When<T>(this T t) => t;
+        public static T Then<T>(this T t) => t;
+        public static T And<T>(this T t) => t;
+    }
+}
